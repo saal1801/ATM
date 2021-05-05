bffb1b696962ed8550a1070f02d8ee5273275649
Sarko <sarko.musik@live.se>
2021-05-05 21:48:24 +0200

    ATM

--- src/ATMProject/Account.java
+++ src/ATMProject/Account.java
@@ -9,6 +9,8 @@ public class Account {
     private int pinNumber;
     private int checkingBalance = 0;
     private int savingBalance = 0;
+    private int [] notes = new int[] {1000, 500, 100};
+    private int [] noteCounter = new int[3];
 
     final Scanner input = new Scanner(System.in);
 
@@ -52,6 +54,29 @@ public class Account {
 
     //business function
     public void getCheckingWithdrawInput() {
+
+        System.out.println("Checking Account Balance: " + checkingBalance);
+        System.out.println("Amount you want to withdraw from Checking Account: ");
+        int amount = input.nextInt();
+
+        //count notes using Greedy approach
+        for (int i = 0; i < 3; i++) {
+            if (amount >= notes[i]) {
+                noteCounter[i] = amount / notes[i];
+                amount = amount - noteCounter[i] * notes[i];
+            }
+        }
+
+        //Print notes
+        for (int i = 0; i < 3; i++) {
+            if (noteCounter[i] != 0){
+                System.out.println(notes[i] + " : " + noteCounter[i]);
+            }
+        }
+
+    }
+
+    /* public void getCheckingWithdrawInput() {
         System.out.println("Checking Account Balance: " + checkingBalance);
         System.out.println("Amount you want to withdraw from Checking Account: ");
         int amount = input.nextInt();
@@ -62,7 +87,7 @@ public class Account {
         }else {
             System.out.println("Balance Cannot be Negative." + "\n");
         }
-    }
+    }*/
 
     public void getSavingWithdrawInput() {
         System.out.println("Saving Account Balance: " + savingBalance);
